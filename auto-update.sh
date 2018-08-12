39463144c11a560416cbc30629b5d8cc0a766a73
alex9446 <alex9446@gmail.com>
2018-08-12 06:30:11 +0200

    12/08/2018

--- auto-update.sh
+++ auto-update.sh
@@ -3,14 +3,19 @@
 cd /config
 touch pidFiles/"$$"
 echo "Aggiunto file pid: $$"
+GIT_CHECK=$(command -v git)
+if [ -z "$GIT_CHECK" ]; then
+  echo "Git non presente. Installo..."
+  apk add git
+fi
 git add auto-update.sh LICENSE automations.yaml configuration.yaml customize.yaml groups.yaml scripts.yaml
 echo "Controllo file modificati"
-STATUS=$(git status --untracked-files=no --porcelain)
-if [ -z "$STATUS" ]; then
+GIT_STATUS=$(git status --untracked-files=no --porcelain)
+if [ -z "$GIT_STATUS" ]; then
   echo "Nessuna modifica"
 else
   echo "File modificati"
-  git commit -m `date +%x`
+  git commit -m `date +%d/%m/%Y`
   echo "Nuovo commit"
   git push origin master
   echo "Upload eseguito"
